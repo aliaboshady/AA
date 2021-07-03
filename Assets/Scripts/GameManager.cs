9721f9df6b38b13bc272b200c36a025b0dde0989
LAPTOP-S5SHF46L\Ali <ali_aboshady@hotmail.com>
2021-07-03 03:41:04 +0100

    Complete project

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour {
+	bool gameHasEnded = false;
+	public Rotator rotator;
+	public Spawner spawner;
+	public Animator animator;
+
+	public void EndGame () {
+		if (gameHasEnded) {
+			return;
+		}
+		gameHasEnded = true;
+		rotator.enabled = false;
+		spawner.enabled = false;
+
+		animator.SetTrigger ("End Game");
+	}
+
+	public void Restart(){
+		SceneManager.LoadScene (SceneManager.GetActiveScene ().buildIndex);
+	}
+}
---
+++ Assets/Scripts/Pin.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pin : MonoBehaviour {
+	public float speed = 20f;
+	public Rigidbody2D rb;
+	bool isPinned = false;
+
+	void Update () {
+		if (!isPinned) {
+			rb.MovePosition (rb.position + Vector2.up * speed * Time.deltaTime);
+		}
+	}
+
+	void OnTriggerEnter2D(Collider2D col){
+		
+		if (col.CompareTag ("Rotator")) {
+			transform.SetParent (col.transform);
+			Score.PinCount++;
+			isPinned = true;
+		}
+
+		if (col.CompareTag ("Pin")) {
+			FindObjectOfType<GameManager>().EndGame();
+		}
+	}
+}
